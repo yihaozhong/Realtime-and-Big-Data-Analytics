6e5c165237b356d2ec176df87e517cbc247b51e5
Yihao Larry <30385362+yihaozhong@users.noreply.github.com>
2023-03-03 16:14:27 -0500

    Project 2 - updated

--- PageRank/pageRankMapper.java
+++ PageRank/pageRankMapper.java
@@ -1,3 +1,27 @@
-public class pageRankMapper {
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class pageRankMapper
+  extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+  @Override
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
     
+    // each line
+    String line = value.toString();
+    // exact and store as an array, matches sequence of one or more whitespace characters.
+    String[] pages =  line.split("\\s+");
+    
+    float number_of_outlinks = pages.length - 2;
+    
+    for(int i = 1; i <= pages.length-2; i++){
+        context.write(new )
+    }
+  }
 }
+// ^^ pageRankMapper
--- PageRank/pageRankReducer.java
+++ PageRank/pageRankReducer.java
@@ -1,3 +1,12 @@
-public class pageRankReducer {
-    
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable; import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class pageRankReducer
+extends Reducer<Text, IntWritable, Text, IntWritable> {
+@Override
+public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+      
+      context.write(key, new IntWritable(maxValue)); 
+    }
 }
